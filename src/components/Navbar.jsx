24e8844af0835a32157e97c31580800e60b0ca39
AryanBabber <aryanbabber12@gmail.com>
2022-12-01 19:21:27 +0530

    made changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { SearchIcon } from "@heroicons/react/solid";
+import github from '../assets/github.png'
 
 const Navbar = () => {
 	function textbox() {
@@ -9,17 +10,22 @@ const Navbar = () => {
 		<div className="h-[6.25rem] bg-slate-300 dark:bg-slate-800 flex items-center justify-between">
 			<div className="mx-4 cursor-default select-none">
 				<a href="../App.jsx">
-					<h1 className="text-6xl">ReBook</h1>
+					<h1 className="text-6xl dark:text-blue-500">ReBook</h1>
 				</a>
 			</div>
 			<div className="">
 				<ul className="flex">
-					<li className="cursor-pointer hover:bg-blue-500 mx-4 rounded-2xl hover:shadow-inner hover:shadow-black select-none">Genre</li>
-					<li className="cursor-pointer hover:bg-blue-500 mx-4 rounded-2xl hover:shadow-inner hover:shadow-black select-none">Recommendations</li>
-					<li className="cursor-pointer hover:bg-blue-500 mx-4 rounded-2xl hover:shadow-inner hover:shadow-black select-none">Get a Random book</li>
+					<li className="cursor-pointer dark:text-white dark:hover:bg-blue-400 hover:bg-blue-500 mx-4 rounded-2xl hover:shadow-inner hover:shadow-black select-none">Genre</li>
+					<li className="cursor-pointer dark:text-white dark:hover:bg-blue-400 hover:bg-blue-500 mx-4 rounded-2xl hover:shadow-inner hover:shadow-black select-none">Recommendations</li>
+					<li className="cursor-pointer dark:text-white dark:hover:bg-blue-400 hover:bg-blue-500 mx-4 rounded-2xl hover:shadow-inner hover:shadow-black select-none">Get a Random book</li>
+					<a href="https://github.com/AryanBabber/Reviewed-books">
+					<li className="cursor-pointer dark:text-white dark:hover:bg-blue-400 hover:bg-blue-500 mx-4 rounded-2xl hover:shadow-inner hover:shadow-black select-none flex gap-5">
+						<img src={github} alt="github logo" className="h-6 w-6" /><span>Contribute to the project</span>
+					</li>
+					</a>
 					<li>
 						<SearchIcon
-							className="w-6 cursor-pointer"
+							className="w-6 cursor-pointer dark:text-white"
 							onClick={textbox}
 						/>
 					</li>
